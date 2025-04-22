937d1e74e47047f3694760907c1999614a49d21c
iantechprojects13 <ian.epidemia13@gmail.com>
2025-04-22 09:55:52 +0800

    added a maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>We'll be back soon!</title>
+    <style>
+        body {
+            background-color: #f0f4f8;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            text-align: center;
+            color: #333;
+        }
+        h1 {
+            font-size: 3rem;
+            margin-bottom: 1rem;
+        }
+        p {
+            font-size: 1.2rem;
+        }
+    </style>
+</head>
+<body>
+    <h1>🚧 Site Under Maintenance</h1>
+    <p>We’re currently performing some updates. Please check back later.</p>
+</body>
+</html>
